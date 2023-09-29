25be84bdbd6ce89d9b54bba335a07bc2817ec310
Michael Barrera <soporte@michaelbarrera.dev>
2023-09-28 18:55:25 -0600

    Add: Svelte

---
+++ src/components/buttons/hamburguer.svelte
@@ -0,0 +1,9 @@
+<script>
+    import HammyButton from "../icons/hamburguer/hamburguer.astro";
+    import HammyClosed from "../icons/hamburguer/hammyClosed.astro";
+</script>
+
+<button on:click id="hammyButton" class="block lg:hidden p-5">
+    <HammyButton />
+    <HammyClosed />
+</button>
