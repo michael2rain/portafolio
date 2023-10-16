22b9c91647e1605ab3e81fe0268b95b7b52a5726
Michael Barrera <soporte@michaelbarrera.dev>
2023-10-15 22:00:32 -0600

    test: 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
@@ -9,4 +11,6 @@ export default defineConfig({
   build: {
     assets: '_[hash]'
   },
+  output: "server",
+  adapter: cloudflare()
 });
