3bf51a1115be55a7b1e159c9db3e4d97dc6394e7
Michael Barrera <soporte@michaelbarrera.dev>
2023-10-16 18:43:19 -0600

    Add: Dynamic routes for posts and proyects

--- src/functions/wordpress.ts
+++ src/functions/wordpress.ts
@@ -24,3 +24,53 @@ export async function wpquery({ query, variables = {} }: WPGraphQLParams) {
 
     return data;
 }
+
+
+export async function fetchPosts({ query, variables = {} }: WPGraphQLParams) {
+    const res = await fetch(`${import.meta.env.WP_URL}`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+    });
+
+    if (!res.ok) {
+        console.error(res);
+        return {};
+    }
+
+    const { data } = await res.json();
+
+    return data.posts.nodes.map((post) => {
+        return post;
+    });
+};
+
+
+export async function fetchProyects({ query, variables = {} }: WPGraphQLParams) {
+    const res = await fetch(`${import.meta.env.WP_URL}`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+    });
+
+    if (!res.ok) {
+        console.error(res);
+        return {};
+    }
+
+    const { data } = await res.json();
+
+    return data.proyects.nodes.map((proyect) => {
+        return proyect;
+    });
+};
